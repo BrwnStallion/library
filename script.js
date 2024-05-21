2b852a56cfd75598f48219c2d5e25f2971878a53
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-05-21 11:29:32 -0700

    feat: add book master object & library function
    Using Object.create() for performance purposes

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // ----------------------- Library App JavaScript ------------------------------
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Pseudocode ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+/* 
+- Write constructor for 'Book' objects. Should have title, author, number of
+  pages, and whether or not it's been read by the user
+- All of the book objects are going to be stored in a library (library aray)
+- User should be able to add books to the library. They won't be adding a book
+  unless they also want to add it to the library
+- Write a function that takes the users input and stores it in a new book, and
+  puts that new book into the library
+    - This function should use the constructor
+*/
 
 
 "use strict";
@@ -16,4 +25,29 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Testing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+const myLibrary = [];
 
+const bookMaster = {
+    // init method for adding properties to books
+    init: function(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        return this; // allows to chain .create with .init
+  },
+}
+
+// Add info method to book master
+bookMaster.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages,` +
+    ` ${this.read}.`;
+};
+
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = Object.create(bookMaster).init(
+            title, author, pages, read);
+
+    myLibrary.push(book);
+}
