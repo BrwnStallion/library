dcafe046e9f5bd85360950dd4497487cb021f2b4
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-05-24 08:45:09 -0700

    refactor: rearrange arguments for year published

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ const myLibrary = [];
 
 const bookMaster = {
     // init method for adding properties to books
-    init: function(title, author, pages, genre, type, yearPublished, read,
+    init: function(title, author, pages, yearPublished, genre, type, read,
       yearRead) {
         this.title = title;
         this.author = author;
         this.pages = pages;
+        this.yearPublished = yearPublished;
         this.genre = genre;
         this.type = type;
-        this.yearPublished = yearPublished;
         this.read = read;
         this.yearRead = yearRead;
         return this; // allows to chain .create with .init
@@ -57,10 +57,10 @@ bookMaster.info = function() {
 };
 
 
-function addBookToLibrary(title, author, pages, genre, type, yearPublished,
-  read, yearRead) {
+function addBookToLibrary(title, author, pages, yearPublished, genre, type,
+    read, yearRead) {
     const book = Object.create(bookMaster).init(
-      title, author, pages, genre, type, yearPublished, read, yearRead);
+      title, author, pages, yearPublished, genre, type, read, yearRead);
 
     myLibrary.push(book);
 }
